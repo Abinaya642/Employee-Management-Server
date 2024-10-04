fd38b3cc665b10062ab2d03eca07d45936f545f4
PortFolio <abinayasakthi942@gmail.com>
2024-10-04 11:40:15 +0530

    update cors url

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ cloudinary.config({
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://employee-management-client.netlify.app/",
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
   })
 );
